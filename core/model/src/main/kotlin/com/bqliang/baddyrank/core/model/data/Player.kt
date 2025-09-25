55b15dae50b39a2f6ca42ba50fcfa3b9d6b1e985
bqliang <bqliang@outlook.com>
2025-09-26 04:08:27 +0800

    change model image type to Any

--- core/model/src/main/kotlin/com/bqliang/baddyrank/core/model/data/Player.kt
+++ core/model/src/main/kotlin/com/bqliang/baddyrank/core/model/data/Player.kt
@@ -6,16 +6,16 @@ package com.bqliang.baddyrank.core.model.data
  *
  * @property id 唯一标识符
  * @property countryAbbreviation 国籍缩写
- * @property countryFlagUrl 国旗图片 URL
- * @property avatarUrl 运动员头像图片 URL
+ * @property countryFlag 国旗图片
+ * @property avatar 运动员头像图片
  * @property chineseName 运动员中文名
  * @property englishName 运动员英文名
  */
 data class Player(
     val id: String,
     val countryAbbreviation: String,
-    val countryFlagUrl: String,
-    val avatarUrl: String,
+    val countryFlag: Any,
+    val avatar: Any,
     val chineseName: String,
     val englishName: String,
 )
