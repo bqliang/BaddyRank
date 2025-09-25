7945e04b38ae499a99dabb0b47a06705d28980ae
bqliang <bqliang@outlook.com>
2025-09-26 02:29:47 +0800

    rename compose ui test library alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
