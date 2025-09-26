23a16f9db15d2b27e5f99a172978068a4fa14f6f
bqliang <bqliang@outlook.com>
2025-09-26 13:45:10 +0800

    Add crawler dependencies

--- crawler/build.gradle.kts
+++ crawler/build.gradle.kts
@@ -5,3 +5,24 @@ plugins {
 application {
     mainClass.set("com.bqliang.baddy.crawler.MainKt")
 }
+
+dependencies {
+    implementation(projects.crawler.data)
+
+    // DeepSeek
+    implementation(libs.aallam.openai.client) // also need one of Ktor's engines.
+    // Json
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.kotlinx.serialization.json)
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    // Ktor
+    implementation(ktorLibs.client.core)
+    implementation(ktorLibs.client.okhttp)
+    implementation(ktorLibs.client.logging)
+    // Jsoup
+    implementation(libs.jsoup)
+
+    // Test
+    testImplementation(libs.kotlin.test)
+}
