c85d910d75a04515687c1543b455094818d65ab1
bqliang <bqliang@outlook.com>
2025-09-28 14:09:47 +0800

    refactor(crawler): 注释掉巡回赛和奥运积分排名类型

--- crawler/src/main/java/com/bqliang/baddy/crawler/model/RankingType.kt
+++ crawler/src/main/java/com/bqliang/baddy/crawler/model/RankingType.kt
@@ -5,13 +5,13 @@ enum class RankingType(val path: String, val description: String) {
     WORLD_RANKING("ranking", "世界排名"),
 
     // 巡回赛排名 - 代码 2
-    TOUR_RANKING("ranking_finals", "巡回赛排名"),
+    // TOUR_RANKING("ranking_finals", "巡回赛排名"),
 
     // 青年排名 - 代码 3
     JUNIOR_RANKING("ranking_junior", "青年排名");
 
     // 奥运积分排名 - 代码 4
-//    OLYMPIC_QUALIFICATION("olympic2024", "奥运积分排名");
+    // OLYMPIC_QUALIFICATION("olympic2024", "奥运积分排名");
 
     val lowercaseName: String
         get() = name.lowercase()
