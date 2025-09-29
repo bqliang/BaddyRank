d2c068f967df29112e182851f24e9409a2986e46
bqliang <bqliang@outlook.com>
2025-09-29 14:36:47 +0800

    feat(data): 注入网络数据源依赖- 在 OfflineFirstBaddyRankRepository 中添加 BaddyRankNetworkDataSource依赖 - 更新构造函数以接收网络数据源实例- 为后续实现网络数据同步功能做准备

--- core/data/src/main/java/com/bqliang/baddyrank/core/data/repository/OfflineFirstBaddyRankRepository.kt
+++ core/data/src/main/java/com/bqliang/baddyrank/core/data/repository/OfflineFirstBaddyRankRepository.kt
@@ -1,10 +1,12 @@
 package com.bqliang.baddyrank.core.data.repository
 
 import com.bqliang.baddyrank.core.database.dao.RankingDao
+import com.bqliang.baddyrank.core.network.BaddyRankNetworkDataSource
 import javax.inject.Inject
 
 internal class OfflineFirstBaddyRankRepository @Inject constructor(
-    private val rankingDao: RankingDao
+    private val rankingDao: RankingDao,
+    private val network: BaddyRankNetworkDataSource,
 ) : BaddyRankRepository {
 
 }
